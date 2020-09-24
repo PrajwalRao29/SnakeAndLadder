4bc2439d194db1ec89f14f9dc18e2a8e9de00edc
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 12:00:36 +0530

    UC3

--- SAL.java
+++ SAL.java
@@ -1,9 +1,28 @@
 
 public class SAL {
 	public static void main (String args[])
-	{
-		System.out.println("Snake and Ladder game played with single player at start position 0");
+	{	int x=10;
+		System.out.println("Snake and Ladder game played with single player at start position 10");
 		int i=(int)(Math.random()*6%6)+1;
-		System.out.println(i);
+		System.out.println("The dice rolled "+i);
+		int option =(int)(Math.random()*10%3);
+		System.out.println("The option is "+option);
+		switch(option)
+		{
+		case 1:System.out.println("LADDDER");
+		if(x+i<101)
+			{
+			x=x+i;
+			System.out.println("The new position is "+x);
+			}
+		break;
+		case 2:System.out.println("SNAKE");
+		if(x-i>-1)
+			{
+			x=x-i;
+			System.out.println("The new position is "+x);
+			}
+		break;
+		}
 	}	
 }
