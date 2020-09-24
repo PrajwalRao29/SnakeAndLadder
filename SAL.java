198d5aa04510739aea6b51b5327e726afa13d20f
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 11:51:44 +0530

    UC2

--- SAL.java
+++ SAL.java
@@ -3,5 +3,7 @@ public class SAL {
 	public static void main (String args[])
 	{
 		System.out.println("Snake and Ladder game played with single player at start position 0");
+		int i=(int)(Math.random()*6%6)+1;
+		System.out.println(i);
 	}	
 }
