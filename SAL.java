0e76096cd0fd7ae8612c3c713d36b7b32e8a4b91
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 12:36:46 +0530

    UC7

--- SAL.java
+++ SAL.java
@@ -1,10 +1,13 @@
+
 public class SAL {
 	public static void main (String args[])
-	{	int x=0;
-		int count=0;
-		System.out.println("Snake and Ladder game played with single player at start position 0");
-		while(x!=100)
+	{	int x=0,y=0;
+	int count=1;
+	while(x!=100 && y!=100)
+	{
+		if(count%2!=0)
 		{
+			System.out.println("PLAYER 1 turn");
 			count++;
 			int i=(int)(Math.random()*6%6)+1;
 			System.out.println("The dice rolls "+i);
@@ -12,27 +15,75 @@ public class SAL {
 			System.out.println("The option is "+option);
 			switch(option)
 			{
-			case 1:System.out.println("LADDDER");
+			case 1:
+			{
+				System.out.println("LADDDER");
 			if(x+i<101)
 			{
 				x=x+i;
-				System.out.println("The new position is "+x);
+				System.out.println("The new position for PLAYER 1 is "+x);
+				count--;
 			}
 			break;
-			case 2:System.out.println("SNAKE");
+			}
+			case 2:
+			{
+				System.out.println("SNAKE");
 			if(x-i>-1)
 			{
 				x=x-i;
-				System.out.println("The new position is "+x);
+				System.out.println("The new position for PLAYER 1 is "+x);
 			}
 			else
 			{
 				x=0;
-				System.out.println("The new position is "+x);
+				System.out.println("The new position for PLAYER 1 is "+x);
 			}
 			break;
 			}
+			default: System.out.println("No Movement");
+			}
 		}
-		System.out.println("The number of times the dice was thrown are:"+count);
+		else
+		{  
+			System.out.println("PLAYER 2 turn");
+			count++;
+			int i=(int)(Math.random()*6%6)+1;
+			System.out.println("The dice rolls "+i);
+			int option =(int)(Math.random()*10%3);
+			System.out.println("The option is "+option);
+			switch(option)
+			{
+			case 1:
+			{
+				System.out.println("LADDDER");
+			if(y+i<101)
+			{
+				y=y+i;
+				System.out.println("The new position for PLAYER 2 is "+y);
+				count--;
+			}
+			break;
+			}
+			case 2:
+			{
+				System.out.println("SNAKE");
+			if(y-i>-1)
+			{
+				y=y-i;
+				System.out.println("The new position for PLAYER 2 is "+y);
+			}
+			else
+			{
+				y=0;
+				System.out.println("The new position for PLAYER 2 is "+y);
+			}
+			break;
+			}
+			default: System.out.println("No Movement");
+			}
+		}	
+	}
+	System.out.println("The winner of the game is PLAYER "+(count%2+1));
 	}
 }
