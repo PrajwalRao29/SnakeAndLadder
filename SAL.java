69820ed7386904f7f1d60fc9d18e8cfafb1b6104
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 12:15:39 +0530

    UC6

--- SAL.java
+++ SAL.java
@@ -1,10 +1,11 @@
-
 public class SAL {
 	public static void main (String args[])
 	{	int x=0;
+		int count=0;
 		System.out.println("Snake and Ladder game played with single player at start position 0");
 		while(x!=100)
 		{
+			count++;
 			int i=(int)(Math.random()*6%6)+1;
 			System.out.println("The dice rolls "+i);
 			int option =(int)(Math.random()*10%3);
@@ -31,6 +32,7 @@ public class SAL {
 			}
 			break;
 			}
-		}	
+		}
+		System.out.println("The number of times the dice was thrown are:"+count);
 	}
 }
